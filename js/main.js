d656921bfe05c60f8a159ac7c82180afa335e707
Saadat123456 <saadatlucky313@gmail.com>
2022-01-25 01:11:41 +0500

    Add Toggle Class Functionality to Ul

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load",()=>{
+    /***`Support For Hash Navigation`
     let hash = location.hash
     const observer = new MutationObserver(()=>{
         console.log(hash);
@@ -10,4 +11,9 @@ window.addEventListener("load",()=>{
         }
     });
     observer.observe(document, {subtree: true, childList: true})
+    */
+   document.querySelector("#menu-btn").addEventListener("click", (event) => {
+       let ul = document.querySelector("#nav-menu");
+       ul.classList.toggle("mobile-nav-menu");
+   })
 })
