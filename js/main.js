7e908fc06c2812e6759afaf9f8892e0598e7e265
JuanLP <juan.luis.palacios.p@gmail.com>
2022-01-24 14:56:11 -0500

    Add link to menu button  and id

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-document.addEventListener("load",()=>{
-    
+window.addEventListener("load",()=>{
+    let hash = location.hash
+    const observer = new MutationObserver(()=>{
+        console.log(hash);
+        console.log(location.hash);
+        if(hash != location.hash){
+            hash = location.hash;
+            console.log(location.hash);
+            //if(hash=="")
+        }
+    });
+    observer.observe(document, {subtree: true, childList: true})
 })
