e9d4d85f1ce55183dc4ba08f69e742ae4861222a
Saadat123456 <saadatlucky313@gmail.com>
2022-01-26 23:03:34 +0500

    Create Project List and ManipulatePopup function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,109 @@ function toggleNavigationMenu() {
   ul.classList.toggle('mobile-nav-menu');
 }
 
+const projectList = [
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+  {
+    name: 'Keeping track of hundreds  of components website',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    'featured-image': 'imgs/Img%20Placeholder.jpg',
+    technologies: [
+      'html',
+      'Bootstrap',
+      'Ruby on rails',
+    ],
+    id: 'keeping-track-of-hundreds-of-components-website',
+    'live-version': 'https://juanlpalacios.github.io/',
+    'repo-link': 'https://github.com/JuanLPalacios/',
+  },
+]
+
+function manipulatePopup(project) {
+  document.querySelector(".modal h1").innerHTML = project.name;
+  document.querySelector(".modal > img").src = project['featured-imagee'];
+  //document.querySelector(".modal .details-heder ul").innerHTML = ;
+  document.querySelector(".modal .content .text") = project.description;
+  document.querySelector(".modal .actions .btn:nth-child(1)").href = project['live-version'];
+  document.querySelector(".modal .actions .btn:nth-child(2)").href = project['repo-link'];
+}
+
 window.addEventListener('load', () => {
   /* // Support For Hash Navigation
   let $hash = window.location.hash;
