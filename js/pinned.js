2495c50a812c92316872208d2b3b85e1e49403c9
JuanLP <juan.luis.palacios.p@gmail.com>
2022-10-21 08:43:23 -0500

    Update snapshot

--- js/pinned.js
+++ js/pinned.js
@@ -1,10 +1,10 @@
 const pinned = [
+  'JuanLPalacios/apprun-leaflet',
   'JuanLPalacios/capstone-project-1',
   'JuanLPalacios/todo-list',
   'JuanLPalacios/Awesome-books-with-ES6',
   'JuanLPalacios/Recipe-app',
   'JuanLPalacios/JuanLPalacios.github.io',
   'JuanLPalacios/The-Botom-App',
-  'JuanLPalacios/apprun-leaflet',
 ];
 export default pinned;
